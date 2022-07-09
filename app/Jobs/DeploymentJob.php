c88da739cd6dba8287f8f2b041afb663f8f22bbd
Brad Singleton <bsingleton.uk@gmail.com>
2022-07-09 06:21:53 -0500

    Webhook WIP

---
+++ app/Jobs/DeploymentJob.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Spatie\WebhookClient\Jobs\ProcessWebhookJob;
+
+class DeploymentJob extends ProcessWebhookJob
+{
+    public function handle()
+    {
+        Log::info('dumping webhook call');
+        Log::info(json_encode($this->webhookCall));
+    }
+}
