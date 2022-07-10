bc6c92738613da3165d8612d08030acfaa3c4575
Brad Singleton <bsingleton.uk@gmail.com>
2022-07-10 07:58:02 -0500

    Testing

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,21 +1,27 @@
-@include('vendor/autoload.php')
+@servers(['main' => 'localhost'])
 
 @setup
-    use \Illuminate\Support\Facades\Log;
-
     $homeDirectory = '~/';
     $projectDirectory = 'webhooks';
     $branch = 'main';
-
-    Log::info('inside setup using log facade');
 @endsetup
 
-@task('deploy-staging')
+@task('deploy-staging', ['on' => 'main'])
     echo 'Testing echo from deploy-staging'
     {{ 'testing blade echo from deploy-staging' }}
     cd {{ $homeDirectory.$projectDirectory }}
     git pull origin {{ $branch }}
     php artisan migrate --force
+@endtask
+
+@task('deploy-local', ['on' => 'main'])
+    echo "Testing log output inside deploy-local task"
+    {{ 'Testing log output inside deploy-local task via blade syntax' }}
+@endtask
+
+@task('test', ['on' => 'main'])
+    cd /home/vagrant/webhook-cd-test
+    ls -l
 @endtask
 
 {{--@before--}}
--- app/Jobs/DeploymentJob.php
+++ app/Jobs/DeploymentJob.php
@@ -10,6 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 use Spatie\WebhookClient\Jobs\ProcessWebhookJob;
+use Symfony\Component\Process\Process;
 
 class DeploymentJob extends ProcessWebhookJob
 {
@@ -21,6 +22,34 @@ class DeploymentJob extends ProcessWebhookJob
         if (config('app.env') === 'staging') {
             Log::info('Running staging deployment script');
             exec('php vendor/bin/envoy run deploy-staging');
+
+            Log::info('trying with process command');
+
+            $process = new Process(['php vendor/bin/envoy run deploy-staging']);
+            $process->run(function ($type, $buffer) {
+                Log::info('Envoy: ' . $buffer);
+            });
+        }
+
+        if (config('app.env') === 'local') {
+            Log::info('Running local script');
+
+            exec('php vendor/bin/envoy run deploy-local');
+
+
+//            $process = new Process(['php vendor/bin/envoy run deploy-local']);
+//            $process = new Process(['pwd'], '/home/vagrant/webhook-cd-test');
+
+            // pwd = /home/vagrant/webhook-cd-test/public
+//            $process = new Process(['pwd']);
+
+//            $process = new Process(['pwd']);
+
+//            $process->setWorkingDirectory('/home/vagrant/webhook-cd-test');
+
+//            $process->run(function ($type, $buffer) {
+//                Log::info('Envoy: ' . $buffer);
+//            });
         }
 
         Log::info('Finished DeploymentJob');
