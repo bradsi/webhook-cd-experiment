e2ea9c03be14074fa360a0bc739d5686481ed373
Brad Singleton <bsingleton.uk@gmail.com>
2022-07-12 00:19:53 -0500

    Update DeploymentJob

--- app/Jobs/DeploymentJob.php
+++ app/Jobs/DeploymentJob.php
@@ -16,16 +16,13 @@ class DeploymentJob extends ProcessWebhookJob
 {
     public function handle()
     {
-        // test push on staging now
         Log::info('Hit DeploymentJob');
 
         if (config('app.env') === 'staging') {
             Log::info('Running staging deployment script');
-//            exec('php vendor/bin/envoy run deploy-staging');
-
             Log::info('trying with process command');
 
-            $process = new Process(['php vendor/bin/envoy run deploy-staging'], '/var/www/staging.jamsoup.com');
+            $process = new Process(['php', 'vendor/bin/envoy', 'run', 'deploy-staging'], '/var/www/staging.jamsoup.com');
             $process->run(function ($type, $buffer) {
                 Log::info('Envoy: ' . $buffer);
             });
@@ -34,22 +31,10 @@ class DeploymentJob extends ProcessWebhookJob
         if (config('app.env') === 'local') {
             Log::info('Running local script');
 
-            exec('php vendor/bin/envoy run deploy-local');
-
-
-//            $process = new Process(['php vendor/bin/envoy run deploy-local']);
-//            $process = new Process(['pwd'], '/home/vagrant/webhook-cd-test');
-
-            // pwd = /home/vagrant/webhook-cd-test/public
-//            $process = new Process(['pwd']);
-
-//            $process = new Process(['pwd']);
-
-//            $process->setWorkingDirectory('/home/vagrant/webhook-cd-test');
-
-//            $process->run(function ($type, $buffer) {
-//                Log::info('Envoy: ' . $buffer);
-//            });
+            $process = new Process(['php', 'vendor/bin/envoy', 'run', 'deploy-local']);
+            $process->run(function ($type, $buffer) {
+                Log::info('Envoy: ' . $buffer);
+            });
         }
 
         Log::info('Finished DeploymentJob');
